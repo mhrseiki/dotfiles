2bf40d94c6b05c03f3fb34c345d4bc314a562945
seiki <241476304+mhrseiki@users.noreply.github.com>
2025-11-12 01:06:33 +0800

    Update preferred initial size of wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
+config.initial_cols = 120
+config.initial_rows = 40
+
 config.font = wezterm.font "MesloLGS Nerd Font Mono"
 config.font_size = 14
 config.color_scheme = "Catppuccin Mocha"
